2cdef376c85cf0af01352d45ff0a33d0351a2f79
bibek <bibek.thapahere@gmail.com>
2017-01-30 02:43:51 +0545

    Added the Company and CompanyRepository

--- src/main/java/hello/Company.java
+++ src/main/java/hello/Company.java
@@ -1,5 +1,50 @@
 package hello;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceConstructor;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="company")
 public class Company {
+	
+	@Id
+	private String id;
+	private String companyName;
+	
+	@DBRef
+	private List<Department> departments=new ArrayList<>();
+	
+	@PersistenceConstructor
+	public Company(String companyName) {
+		super();
+		this.companyName = companyName;
+	}
+	public Company(){}
+	public String getCompanyName() {
+		return companyName;
+	}
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	public List<Department> getDepartments() {
+		return departments;
+	}
+	public void setDepartments(List<Department> departments) {
+		this.departments = departments;
+	}
+	@Override
+	public String toString() {
+		return "Company [id=" + id + ", companyName=" + companyName + ", departments=" + departments + "]";
+	}
+	
+	
+	
+	
+	
+	
 
 }
